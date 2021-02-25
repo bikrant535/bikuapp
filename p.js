7e5f2401d97e540422b563e88086347a10a20215
Bikrant <bikrant.nepal1@gmail.com>
2021-02-25 08:14:42 +0545

    initial commit

---
+++ p.js
@@ -0,0 +1,4 @@
+function BAGMATIRIVER()
+{
+    alert("The largest temple complex in Nepal, it stretches on both sides of the Bagmati River which is considered holy by Hindus.The main pagoda style temple has a gilded roof, four sides covered in silver, and exquisite wood carvings.Temples dedicated to several other Hindu and Buddhist deities surround the the temple of Pashupatinath");
+}
